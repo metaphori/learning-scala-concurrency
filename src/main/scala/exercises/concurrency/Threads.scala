489452cbcaa09e66ea455beadfac5c258e6ebfac
Roberto Casadei <robyonrails@gmail.com>
2019-11-21 00:27:38 +0100

    Reordering

--- src/main/scala/exercises/concurrency/Threads.scala
+++ src/main/scala/exercises/concurrency/Threads.scala
@@ -50,3 +50,23 @@ object AvoidRaceConds extends App {
   }
   println(k)
 }
+
+object ReorderingBug extends App {
+  for (i <- 0 until 100000) {
+    var a = false; var b = false; var x = -1; var y = -1
+    val t1 = thread { a = true; y = if (b) 0 else 1 }
+    val t2 = thread { b = true; x = if (a) 0 else 1 }
+    t1.join(); t2.join()
+    assert(!(x == 1 && y == 1), s"x = $x, y = $y")
+  }
+}
+
+object ReorderingBugFixed extends App {
+  for (i <- 0 until 100000) {
+    var a = false; var b = false; var x = -1; var y = -1
+    val t1 = thread { this.synchronized{ a = true }; y = if (b) 0 else 1 }
+    val t2 = thread { this.synchronized{ b = true }; x = if (a) 0 else 1 }
+    t1.join(); t2.join()
+    assert(!(x == 1 && y == 1), s"x = $x, y = $y")
+  }
+}
